32e53171dff9f07041650dda43c1e0804045eab5
mikes <mike@theweblife.com>
2016-09-28 22:28:00 -0500

    first go round

---
+++ js/combined.js
@@ -0,0 +1,8 @@
+// @codekit-prepend "../bower_components/jquery/dist/jquery.min.js";
+// @codekit-prepend "../bower_components/angular/angular.min.js";
+// @codekit-prepend "../bower_components/angular-ui-router/release/angular-ui-router.min.js";
+// @codekit-prepend "app.js";
+// @codekit-prepend "controllers.js";
+// @codekit-prepend "services.js";
+// @codekit-prepend "directives.js";
+// @codekit-prepend "common.js";
